e84db00ddd534688ebf07a8c7c2e943aad7310ce
Leandro Ramirez <leandrosramirez@gmail.com>
2022-06-29 15:16:51 -0300

    modificación creación de conf  nginx
    modificación de la línea de creación de archivo configuración en nginx

--- nginxproxyreversoycerbot.sh
+++ nginxproxyreversoycerbot.sh
@@ -1,11 +1,14 @@
 #// Instalar nginx, agregar la configuración de proxy reverso
 apt-get install nginx
-nano /etc/nginx/sites-available/dominioreverso.conf
+cd /etc/nginx/sites-available/
+>dominioreverso.conf
 echo server { >> /etc/nginx/sites-available/dominioreverso.conf
-echo server_name dominioreverso.com; >> /etc/nginx/sites-available/dominioreverso.conf
+echo server_name maxseguridad.com.ar; >> /etc/nginx/sites-available/dominioreverso.conf
+echo server_name www.maxseguridad.com.ar; >> /etc/nginx/sites-available/dominioreverso.conf
 echo location / { >> /etc/nginx/sites-available/dominioreverso.conf
 echo proxy_pass      http://127.0.0.1:8069/; >> /etc/nginx/sites-available/dominioreverso.conf
-echo }}>> /etc/nginx/sites-available/dominioreverso.conf
+echo } >> /etc/nginx/sites-available/dominioreverso.conf
+echo } >> /etc/nginx/sites-available/dominioreverso.conf
 ln -s /etc/nginx/sites-available/dominioreverso.conf /etc/nginx/sites-enabled/dominioreverso.conf
 service nginx restart
 #// Instalar cerboot
